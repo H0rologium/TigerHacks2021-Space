2654dcbf2cb3de92e061cafeeb823605373893f7
ulpiana <ulpiana.b@hotmail.com>
2021-11-06 23:43:09 -0500

    "stop ignoring me >:(" -build

--- EarthSite/js/assetLoader.js
+++ EarthSite/js/assetLoader.js
@@ -1,5 +1,5 @@
 // import { OrbitControls } from "../js/three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "../js/three/examples/jsm/loaders/GLTFLoader.js";
+// import { GLTFLoader } from "../js/three/examples/jsm/loaders/GLTFLoader.js";
 // import { RGBELoader } from "../js/three/examples/jsm/loaders/RGBELoader.js";
 import { RoughnessMipmapper } from "../js/three/examples/jsm/utils/RoughnessMipmapper.js";
 
